492bf4cbd56f1abbe236c856a662ffedb40ab6fd
adityak14 <aditya.kaushik1213@gmail.com>
2022-09-03 19:41:45 -0400

    Base Code Setup

---
+++ server/controllers/posts.js
@@ -0,0 +1,25 @@
+import Posts from "../models/postMessage.js";
+
+export const getPosts = async (req,res)=>{
+    try{
+        const posts = await Posts.find()
+
+        console.log(posts);
+        res.status(200).json(posts);
+
+    }catch(error){
+        res.status(404).json({message: error.message});
+    }
+}
+
+export const createPost = async (req,res)=>{
+    const post = req.body;
+    const newPost = new Posts(post);
+    try {
+
+        await newPost.save()
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({message: error.message});
+    }
+}
