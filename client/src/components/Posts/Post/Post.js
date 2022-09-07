f2c4a1e2d2136784bc06a954424fa7aed0c8d9cd
adityak14 <aditya.kaushik1213@gmail.com>
2022-09-07 19:15:58 -0400

    Like Post functionality added.

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -7,7 +7,7 @@ import moment from 'moment';
 import { useDispatch } from 'react-redux';
 import useStyles from './styles';
 
-import { deletePost } from '../../../actions/action_posts';
+import { deletePost, likePost } from '../../../actions/action_posts';
 
 const Post = ({ post, setCurrentId }) => {
   const dispatch = useDispatch();
@@ -32,8 +32,8 @@ const Post = ({ post, setCurrentId }) => {
       </CardContent>
       <CardActions className={classes.cardActions}>
         <Button size="small" color="primary" 
-        // onClick={() => dispatch(likePost(post._id))}
-        onClick={() => console.log("Like button")}
+        onClick={() => dispatch(likePost(post._id))}
+        // onClick={() => console.log("Like button")}
         ><ThumbUpAltIcon fontSize="small" /> Like {post.likeCount} </Button>
         <Button size="small" color="primary" 
         onClick={() => dispatch(deletePost(post._id))}
