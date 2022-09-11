ef740977b5c15e2c35c21b5c50318a3ca1e6d2e1
aditya0sehgal <aditya0sehgal.dev@gmail.com>
2022-09-11 17:46:14 -0400

    Sign up & Sign In forms added

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,23 @@
 import React, { useState, useEffect }  from 'react';
 import './App.css';
-import { getPosts } from './actions/action_posts'
-import { Container, AppBar, Grow, Grid } from '@material-ui/core';
-import Form from './components/Form/Form';
-import Posts from './components/Posts/Posts';
-import useStyles from './styles'
-import { useDispatch } from 'react-redux';
+import { Container, Grow, Grid } from '@material-ui/core';
 import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Auth from './components/Auth/Auth';
 
 function App() {
-  const classes = useStyles();
-  const dispatch = useDispatch();
-  const [currentId, setCurrentId] = useState(0);
-
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch])
   
   return (
-    <>
+    <BrowserRouter>
       <Container maxWidth="lg">
             <Navbar />
-              <Grow in>
-                <Container>
-                  <Grid className={classes.mainContainer} container justifyContent='space-between' alignItems='stretch' spacing={3}>
-                    <Grid item xs={12} sm={7}>
-                      <Posts setCurrentId={setCurrentId}/>
-                    </Grid>
-                    <Grid item xs={12} sm={4}>
-                      <Form currentId={currentId} setCurrentId={setCurrentId}/>
-                    </Grid>
-                  </Grid>
-                </Container>
-              </Grow>
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/auth" exact component={Auth} />
+            </Switch>
       </Container>
-    </>
+    </BrowserRouter>
   );
 }
 
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,16 +1,32 @@
 import React from 'react'
-import {Grow, AppBar} from '@material-ui/core';
+import {Grow, AppBar, Toolbar, Avatar, Typography, Button} from '@material-ui/core';
 import useStyles from './styles';
 import {Link} from 'react-router-dom';
 
 export default function Navbar() {
-    const classes = useStyles()
+  const classes = useStyles()
+  const user = null;
   return (
     <Grow in>
         <AppBar className={classes.appBar} position='static' color='inherit'>
             <div className={classes.brandContainer}>
-                <img component={Link} className={classes.image} src="https://www.squadhelp.com/story_images/visual_images/1652391964-TravelBook-1.jpg" alt='TravelBook' align='center' height='150'/>
+                <img className={classes.image} src="https://www.squadhelp.com/story_images/visual_images/1652391964-TravelBook-1.jpg" alt='TravelBook' align='center' height='150'/>
             </div>
+            <Toolbar className={classes.toolbar}>
+              {user ?
+                (<div className={classes.profile}>
+                  <Avatar className={classes.purple} src={user.result.imageUrl} alt={user.result.name}>
+                    {user.result.name.charAt(0)}
+                  </Avatar>
+                  <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
+                  <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
+                </div>)
+              :
+                (
+                  <Button component={Link} to="/auth" variant="contained" color="primary">Sign-In</Button>
+                )
+              }
+            </Toolbar>
         </AppBar>
     </Grow>
   )
